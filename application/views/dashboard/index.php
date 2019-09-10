deaad715d240817caba440ba615810cf55536d38
Afiefsky <afief.farista@fore.coffee>
2019-09-10 20:38:46 +0700

    Progress on admin chat

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,16 +1,36 @@
-<h3>Daftar Kontak</h3>
+<h3>Pilih Layanan</h3>
 <br>
 <br>
+
 <table class="table table-bordered">
-  <thead>
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama Layanan</th>
+            <th>Icon</th>
+            <th>Opsi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>R & D</td>
+            <td>-icon here-</td>
+            <td><button class="btn btn-sm btn-info">Pilih</button></td>
+        </tr>
+    </tbody>
+</table>
+
+<!-- <table class="table table-bordered">
+<thead>
     <tr>
-      <th>No</th>
-      <th>Nama</th>
-      <th>Status</th>
-      <th>Aksi</th>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Status</th>
+        <th>Aksi</th>
     </tr>
-  </thead>
-  <tbody>
+</thead>
+<tbody>
     <?php
     $no = 0;
     $logged_status = 0;
@@ -24,24 +44,24 @@
         }
 
         echo "<tr>
-          <td>$no</td>
-          <td>$r->first_name $r->last_name</td>
-          <td>$logged_status</td>
-          <td>".anchor('chat/redirect/'.$this->session->userdata('user_id').'/'.$r->id, 'Chat', ['class' => 'btn btn-primary btn-sm']).'</td>
+            <td>$no</td>
+            <td>$r->first_name $r->last_name</td>
+            <td>$logged_status</td>
+            <td>".anchor('chat/redirect/'.$this->session->userdata('user_id').'/'.$r->id, 'Chat', ['class' => 'btn btn-primary btn-sm']).'</td>
         </tr>';
     }
     ?>
-  </tbody>
-</table>
+    </tbody>
+</table> -->
 </p>
 
 <!-- <p>
-  <table width="100%">
+    <table width="100%">
     <tr>
-      <td align="center" style="background: #529a33;">
+        <td align="center" style="background: #529a33;">
         <a href="https://wa.me/62895343775002" style="color: #ffffff;">Live Chat Whatapp, Klik Di Sini</a>
-      </td>
+        </td>
     </tr>
-  </table>
+    </table>
 </p> -->
 <!-- <script src="<?php echo base_url(); ?>js/dashboard.js" type="text/javascript"></script> -->
